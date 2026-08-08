5d39a65b8261ab8bdbf2929e228d4afe672be59a
Narendra Pal <narenderpal.singhnsr@gmail.com>
2026-08-08 18:00:23 +0530

    err handle

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for a named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// if no name given return an error with msg
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// if name is provided than
 	// Return a greeting that embeds the name in a message.
 	greets := "Nice meeting you"
-	return fmt.Sprintf("%s, %s", greets, name)
+	message := fmt.Sprintf("%s, %s", greets, name)
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	// remove extra logs(such as date, time) provided by log package and rather use greetings as prefix
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-	message := greetings.Hello("arora")
+	fmt.Println("Hello, world!") // fmt.Println, prints to stdout
+
+	message, err := greetings.Hello("")
+	// if error returned than print it to console and exit program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// else if no error was returned than print msg to console
 	fmt.Println(message)
 
 }
