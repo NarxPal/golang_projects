c3c4e48479d6b6ce9e3f12aaa58247b061e7e15a
Narendra Pal <narenderpal.singhnsr@gmail.com>
2026-09-18 18:18:17 +0530

    handle duplicate url shortid

--- url-shortener/main.go
+++ url-shortener/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand/v2"
 	"net/http"
 	"net/url"
 )
@@ -11,6 +12,12 @@ import (
 type ShortUrl struct {
 	URL string `json:"url"`
 }
+
+// urlStore stores id -> url
+var urlStore = make(map[string]string)
+
+// idStore stores url -> id (to find duplicate id's for single url)
+var idStore = make(map[string]string)
 
 func homeHandler(writer http.ResponseWriter, requestData *http.Request) {
 	fmt.Fprintf(writer, "http server")
@@ -20,8 +27,14 @@ func healthHandler(writer http.ResponseWriter, requestData *http.Request) {
 	fmt.Fprint(writer, "u are on health page")
 }
 
-func generateShortId() {
-
+func generateShortId() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	emptyId := make([]byte, 6)
+	for i := range emptyId {
+		emptyId[i] = charset[rand.IntN(len(charset))]
+	}
+	randomStr := string(emptyId)
+	return randomStr
 }
 
 func shortenHandler(w http.ResponseWriter, r *http.Request) {
@@ -49,10 +62,24 @@ func shortenHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	generateShortId()
+	shortId, exists := idStore[fetchUrl.URL]
 
+	if exists {
+		fmt.Fprint(w, shortId)
+		fmt.Printf("map check %v\n", urlStore)
+		return
+	}
+	randShortId := generateShortId()
+	fmt.Printf("short id : %v\n", randShortId)
+	urlStore[randShortId] = fetchUrl.URL
+	idStore[fetchUrl.URL] = randShortId
+
+	fmt.Fprint(w, randShortId)
+	fmt.Printf("map check %v\n", urlStore)
 	fmt.Printf("u check %v\n", u.Scheme)
-	fmt.Fprint(w, fetchUrl.URL, " post req recieved")
+	fmt.Fprint(w, " post req recieved")
+
+	// after creating a short id , just map the id wrt url
 
 }
 
